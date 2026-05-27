4346a0540077502b4df8a02b1ff18d17bd288d6a
Tertuliano Barbosa <arazaodetudoeoamor@gmail.com>
2026-05-27 15:41:43 -0300

    Fix: Added missing dart:math import to resolve build error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:js_interop';
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:google_fonts/google_fonts.dart';
