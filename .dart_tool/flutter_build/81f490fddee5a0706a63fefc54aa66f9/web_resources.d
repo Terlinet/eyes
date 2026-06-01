e6d18ba97902b4a207fa85eb347d0e08cb349b10
Tertuliano Barbosa <arazaodetudoeoamor@gmail.com>
2026-06-01 19:22:25 -0300

    Cleanup: remove ignored temporary files and keep only source code

--- .dart_tool/flutter_build/81f490fddee5a0706a63fefc54aa66f9/web_resources.d
+++
@@ -1,1 +0,0 @@
- D:\\TERLINET\ GOVERNANCE\\TERLINET_ALARM\\build\\web\\previeweyes.png:  D:\\TERLINET\ GOVERNANCE\\TERLINET_ALARM\\web\\index.html D:\\TERLINET\ GOVERNANCE\\TERLINET_ALARM\\web\\previeweyes.png
